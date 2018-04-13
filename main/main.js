24cc14a9a04790adc61bc4e81a751b9426895263
liukxaa <644306737@qq.com>
2018-04-13 13:45:31 +0800

    重构 增加可读性

--- main/main.js
+++ main/main.js
@@ -1,10 +1,43 @@
-function get_fee(km, wait_min) {
-    if (km <= 2) {
-        return Math.round(6 + wait_min * 0.25);
-    } else if (km <= 8) {
-        return Math.round(6 + (km - 2) * 0.8 + wait_min * 0.25);
+const FEE_FOR_START = 6;
+const FEE_FOR_WAIT_PER_MIN = 0.25;
+const FEE_FOR_NORMAL_PER_KM = 0.8;
+const FEE_FOR_EXTRA = FEE_FOR_NORMAL_PER_KM + FEE_FOR_NORMAL_PER_KM * 0.5;
+
+const DISTANCE_FOR_FREE = 2;
+const DISTANCE_FOR_EXTRA_FEE = 8;
+
+function is_less_than_free_distance(distance) {
+    return distance <= DISTANCE_FOR_FREE;
+}
+
+function is_less_than_extra_fee_distance(distance) {
+    return distance <= DISTANCE_FOR_EXTRA_FEE;
+}
+
+function get_fee_for_normal_distance(distance) {
+    if (distance > DISTANCE_FOR_EXTRA_FEE) {
+        distance = DISTANCE_FOR_EXTRA_FEE;
+    }
+    return (distance - DISTANCE_FOR_FREE)  * FEE_FOR_NORMAL_PER_KM;
+}
+
+function get_fee_for_wait(wait_min) {
+    return wait_min * FEE_FOR_WAIT_PER_MIN;
+}
+
+function get_fee_for_extra_distance(distance) {
+    return (distance - DISTANCE_FOR_EXTRA_FEE) * FEE_FOR_EXTRA;
+}
+
+function get_fee(distance, wait_min) {
+    if (is_less_than_free_distance(distance)) {
+        return Math.round(FEE_FOR_START + get_fee_for_wait(wait_min));
+    } else if (is_less_than_extra_fee_distance(distance)) {
+        return Math.round(FEE_FOR_START + get_fee_for_normal_distance(distance)
+            + get_fee_for_wait(wait_min));
     } else {
-        return Math.round(6 + 6 * 0.8 + (km - 8) * 1.2 + wait_min * 0.25);
+        return Math.round(FEE_FOR_START + get_fee_for_normal_distance(distance)
+            + get_fee_for_extra_distance(distance) + get_fee_for_wait(wait_min));
     }
 }
 
