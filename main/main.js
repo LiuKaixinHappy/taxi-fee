d62c8137256db637d98d628f3dd09846c705da27
liukxaa <644306737@qq.com>
2018-04-13 11:45:13 +0800

    完成 2公里内停车任意分钟计价

--- main/main.js
+++ main/main.js
@@ -1,4 +1,7 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+function get_fee(km, wait_min) {
+    if (km <= 2) {
+        return 6 + wait_min * 0.25;
+    }
+}
+
+module.exports = {get_fee};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,13 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('should 6 yuan with 1.8km without stop', function () {
+        let fee = main.get_fee(1.8, 0);
+        expect(fee).toEqual(6);
+    });
+
+    it('should 6 yuan with 1.8km and wait for 1min', function () {
+        let fee = main.get_fee(1.8, 1);
+        expect(fee).toEqual(6.25);
+    });
 });
