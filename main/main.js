73b5a44effaea53e55a075d8b30d30a2616571d2
liukxaa <644306737@qq.com>
2018-04-13 11:56:50 +0800

    完成 全部需求

--- main/main.js
+++ main/main.js
@@ -1,6 +1,10 @@
 function get_fee(km, wait_min) {
     if (km <= 2) {
-        return 6 + wait_min * 0.25;
+        return Math.round(6 + wait_min * 0.25);
+    } else if (km <= 8) {
+        return Math.round(6 + (km - 2) * 0.8 + wait_min * 0.25);
+    } else {
+        return Math.round(6 + 6 * 0.8 + (km - 8) * 1.2 + wait_min * 0.25);
     }
 }
 
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -6,8 +6,18 @@ describe('taxi fee', function () {
         expect(fee).toEqual(6);
     });
 
-    it('should 6 yuan with 1.8km and wait for 1min', function () {
-        let fee = main.get_fee(1.8, 1);
-        expect(fee).toEqual(6.25);
+    it('should 7 yuan with 1.8km and wait for 3min', function () {
+        let fee = main.get_fee(1.8, 3);
+        expect(fee).toEqual(7);
+    });
+
+    it('should 10 yuan with 3.5 km and wait for 10min', function () {
+        let fee = main.get_fee(3.5, 10);
+        expect(fee).toEqual(10);
+    });
+
+    it('should 17 yuan with 10km and wait for 15min', function () {
+        let fee = main.get_fee(10, 15);
+        expect(fee).toEqual(17);
     });
 });
